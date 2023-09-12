9926e18ac2e4b6f79d86463c00a75df4971dd5d4
Abourache <abouracheibrahime17@gmail.com>
2023-09-12 12:29:54 -0700

    CPython #0: Python lists

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,26 +1,30 @@
+#include "lists.h"
+
 /**
  * is_palindrome - checks if a linked list is a palindrome
+ *
  * @head: the head address of the linked list
+ *
  * Return: 1 if it's a palindrome, 0 else
  */
 
 int is_palindrome(listint_t **head)
 {
 	listint_t *cur = *head;
-	int tab[2048], x = 0, y = 0;
+	int tab[2048], a = 0, y = 0;
 
 	if (*head)
 	{
 		while (cur)
 		{
-			tab[x] = cur->n;
+			tab[a] = cur->n;
 			cur = cur->next;
-			x++;
+			a++;
 		}
 
-		while (y < x / 2)
+		while (y < a / 2)
 		{
-			if (tab[y] == tab[x - y - 1])
+			if (tab[y] == tab[a - y - 1])
 				y++;
 			else
 				return (0);
